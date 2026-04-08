1e7dd49ae42bd2308c68d52e431e1bf3997ce5d0
bouzuya <m@bouzuya.net>
2026-04-08 21:29:50 +0900

    Add UserId

--- crates/main/src/model.rs
+++ crates/main/src/model.rs
@@ -1,5 +1,6 @@
 pub(crate) mod date_time;
 mod user;
+mod user_id;
 
 pub(crate) use self::date_time::DateTime;
 pub(crate) use self::user::FirestoreUserRepository;
@@ -7,3 +8,4 @@ pub(crate) use self::user::FirestoreUserRepository;
 pub(crate) use self::user::InMemoryUserRepository;
 pub(crate) use self::user::User;
 pub(crate) use self::user::UserRepository;
+pub(crate) use self::user_id::UserId;
