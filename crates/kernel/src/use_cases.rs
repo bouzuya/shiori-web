70b6248dca54a6f640723c6c4c7ee717c5a1f91e
bouzuya <m@bouzuya.net>
2026-06-12 11:34:08 +0900

    Add UserSettingsReader

--- crates/kernel/src/use_cases.rs
+++ crates/kernel/src/use_cases.rs
@@ -1,7 +1,9 @@
 mod bookmark_reader;
 mod bookmark_repository;
 mod user_repository;
+mod user_settings_reader;
 
 pub use self::bookmark_reader::BookmarkReader;
 pub use self::bookmark_repository::BookmarkRepository;
 pub use self::user_repository::UserRepository;
+pub use self::user_settings_reader::UserSettingsReader;
---
+++ crates/kernel/src/use_cases/user_settings_reader.rs
@@ -0,0 +1,8 @@
+#[async_trait::async_trait]
+pub trait UserSettingsReader: Send + Sync {
+    /// 指定ユーザーの設定を取得する。未保存なら `None`。
+    async fn get(
+        &self,
+        user_id: crate::entities::UserId,
+    ) -> anyhow::Result<Option<crate::read_models::UserSettingsView>>;
+}
