b5e9bad8a8b07e87c1f40e44f04bfe99da77a40d
bouzuya <m@bouzuya.net>
2026-06-14 23:33:04 +0900

    Update devcontainer settings

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 curl -fsSL https://claude.ai/install.sh | bash
+curl -fsSL https://direnv.net/install.sh | bash
+echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
 
 rustup component add --toolchain nightly rustfmt
 rustup component add llvm-tools-preview
