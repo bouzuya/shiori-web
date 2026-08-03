92eb1648a3283cca0e474674f7f2ba2ee271ba76
bouzuya <m@bouzuya.net>
2026-08-03 20:06:07 +0900

    Rename export_cache mod to export_cache_store mod

--- crates/cli/src/storage.rs
+++ crates/cli/src/storage.rs
@@ -1,9 +1,9 @@
 mod config_store;
-mod export_cache;
+mod export_cache_store;
 mod token_store;
 
 pub(crate) use self::config_store::ConfigJson;
 pub(crate) use self::config_store::ConfigStore;
-pub(crate) use self::export_cache::ExportCacheStore;
+pub(crate) use self::export_cache_store::ExportCacheStore;
 pub(crate) use self::token_store::StoredToken;
 pub(crate) use self::token_store::TokenStore;
